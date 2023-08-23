fe52e3f69e671d25950f7f4bf3c6830630e19022
ShSato4JPN <shsato4.private.1221@gmail.com>
2023-08-23 23:49:42 +0900

    feat: add contentful host info

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+        port: "",
+        pathname: "/gwzlwafe3hu0/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
