360f6f5974344153c81d03d51b7502651ba92ddf
ShSato4JPN <shsato4.private.1221@gmail.com>
2023-09-04 23:50:13 +0900

    feat: add router of about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AboutTop from "@/components/AboutTop";
+
+async function Page(): Promise<JSX.Element> {
+  return <AboutTop />;
+}
+
+export default Page;
---
+++ src/components/AboutTop/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+import styles from "./style.module.scss";
+import Image from "next/image";
+import Link from "next/link";
+import Separator from "@/components/Separator";
+import { AiFillInstagram, AiFillTwitterCircle } from "react-icons/ai";
+
+function AboutTop(): JSX.Element {
+  return (
+    <div className={styles["wrapper"]}>
+      <main className={styles["container"]}>
+        <header className={styles["header"]}>
+          <figure className={styles["header__icon"]}>
+            <Image
+              src="/user-icon.svg"
+              alt={"管理人のイメージ画像です"}
+              width={150}
+              height={150}
+            />
+          </figure>
+          <div className={styles["header__name"]}>
+            <span>サトシ</span>
+          </div>
+        </header>
+        <div className={styles["body"]}>
+          <div className={styles["body__text"]}>
+            <p>
+              都内で Web エンジニアとして働いているサトシです。
+              <br />
+              このブログは日々の学びをアウトプットするために作成しました。
+              <br />
+              技術的なこと以外にも、日常や趣味についても書いていこうと思います。
+              <br />
+            </p>
+          </div>
+          <div className={styles["body__sns"]}>
+            <a href="/">
+              <AiFillInstagram
+                color="orange"
+                size={40}
+                className={styles["body__sns-instagram"]}
+              />
+            </a>
+            <a href="/">
+              <AiFillTwitterCircle
+                color="orange"
+                size={40}
+                className={styles["body__sns-twitter"]}
+              />
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default AboutTop;
